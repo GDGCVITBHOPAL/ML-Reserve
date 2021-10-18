735f0f3b9392c61dc7d774660abf450cb95fd0e5
Anant chauhan <63709820+Anant1711@users.noreply.github.com>
2021-10-18 11:56:13 +0530

    Update dice_rolling.py

--- Python-Scripts/Dice_Rolling/dice_rolling.py
+++ Python-Scripts/Dice_Rolling/dice_rolling.py
@@ -2,18 +2,20 @@ import random
 import time
 
 x = "y"
+# Gnenerates a random number
+# between 1 and 6 
 
-    # Gnenerates a random number
-    # between 1 and 6 (including
-    # both 1 and 6)
-    str = "Rolling..."
-    strp = str.center(93)
-    print("\n")
-    print(strp)
-    print("\n")
-    time.sleep(0.5)
+
+str = "Rolling..."
+strp = str.center(93)
+print("\n")
+print(strp)
+print("\n")
+time.sleep(0.5)
+
+
+def rolling():
     no = random.randint(1, 6)
-
     if no == 1:
         print("\t\t\t\t\t __________")
         print("\t\t\t\t\t|          |")
@@ -57,5 +59,9 @@ x = "y"
         print("\t\t\t\t\t|0       0 |")
         print("\t\t\t\t\t|__________|")
 
+
+while True:
+    if(x == 'y' or 'Y'):
+        rolling()
     x = input("Press (y) to roll again and for exit press any other key: ")
-    print("\n")
+print("\n")
